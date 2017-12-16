eabc0391e0b0cd8b8b849d53abe6404e5a152dbb
Dom <dominic@dom.gs>
2017-12-16 15:50:10 +0000

    Build update

--- build/build.sh
+++ build/build.sh
@@ -10,19 +10,18 @@ while read m; do
   [[ -z "${m// }" ]] && continue;
   
   MAP=$( echo "$m" | tr -cd 'A-Za-z0-9._-' )
-  
   MISSION_FOLDER="./build/$MISSION_FOLDER_NAME.$MAP"
 
   echo -e "Building $MAP...";
   
-  mkdir -p "$MISSION_FOLDER"
+  mkdir -p "$MISSION_FOLDER" > /dev/null
   
-  cp -r ./src/* "$MISSION_FOLDER"
+  cp -r ./src/* "$MISSION_FOLDER" > /dev/null
   
   cat "$MISSION_FOLDER/mission.sqm" | envhandlebars | tee "$MISSION_FOLDER/mission.sqm" > /dev/null
   cat "$MISSION_FOLDER/description.ext" | envhandlebars | tee "$MISSION_FOLDER/description.ext" > /dev/null
   
-  ./tools/bin/makepbo "$MISSION_FOLDER" "./bin/$MISSION_NAME.$MAP.pbo"
+  ./tools/bin/makepbo "$MISSION_FOLDER" "./bin/$MISSION_NAME.$MAP.pbo" > /dev/null
   
   echo -e "Building $MAP done.";
   
