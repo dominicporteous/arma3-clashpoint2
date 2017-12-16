7e6f007751234d49d20f1d4984b4fad56aab002a
Dom <dominic@dom.gs>
2017-12-16 15:22:44 +0000

    Initial travis run

---
+++ build/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+MISSION_NAME="Clashpoint"
+MISSION_FOLDER_NAME="$MISSION_NAME"
+
+while read m; do
+
+  [[ -z "${m// }" ]] && continue;
+  
+  MAP=$( echo "$m" | tr -cd 'A-Za-z0-9._-' )
+  
+  MISSION_FOLDER="./build/$MISSION_FOLDER_NAME.$MAP"
+
+  echo -e "Building $MAP...";
+  
+  mkdir -p "$MISSION_FOLDER"
+  
+  cp -r ./src/* "$MISSION_FOLDER"
+  
+  cat "$MISSION_FOLDER/mission.sqm" | envhandlebars | tee "$MISSION_FOLDER/mission.sqm"
+  cat "$MISSION_FOLDER/description.ext" | envhandlebars | tee "$MISSION_FOLDER/description.ext"
+  
+  ./tools/makepbo "$MISSION_FOLDER" "./bin/$MISSION_NAME.$MAP.pbo"
+  
+  echo -e "Building $MAP done.";
+  
+done < ./build/maplist.txt
