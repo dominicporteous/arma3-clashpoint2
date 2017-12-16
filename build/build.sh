cf97b36453aeb7e7eb643feed73dd323a3f3a875
dominicporteous <dominic@dom.gs>
2017-12-16 17:12:08 +0000

    Travis ci (#1)
    * Initial travis run

---
+++ build/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir -p "bin"
+
+MISSION_NAME="Clashpoint"
+MISSION_FOLDER_NAME="$MISSION_NAME"
+
+while read m; do
+
+  [[ -z "${m// }" ]] && continue;
+  
+  export MAP=$( echo "$m" | tr -cd 'A-Za-z0-9._-' )
+  export MISSION_FOLDER="./build/$MISSION_FOLDER_NAME.$MAP"
+
+  echo -e "Building $MAP..." && echo -en "travis_fold:start:build.map.$MAP\\r"
+
+  mkdir -p "$MISSION_FOLDER"
+  cp -r ./src/* "$MISSION_FOLDER"
+  
+  cat "$MISSION_FOLDER/mission.sqm" | ./tools/bin/mo > "$MISSION_FOLDER/mission.sqm"
+  cat "$MISSION_FOLDER/description.sqm" | ./tools/bin/mo > "$MISSION_FOLDER/description.sqm"
+  
+  ./tools/bin/makepbo "$MISSION_FOLDER" "./bin/$MISSION_NAME.$MAP.pbo"
+  
+  echo -e "Building $MAP done." && echo -en "travis_fold:end:build.map.$MAP\\r"
+  
+done < ./build/maplist.txt
+
+echo -en "travis_fold:start:end_debug\\r"
+[[ "$DEBUG" != "" ]] && tree -a || exit 0
+echo -en "travis_fold:end:end_debug\\r"
